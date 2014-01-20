4084d6b4eeeefd05f3341f1c84572e53f31ef7d8
Trevor <trevorjbaron@gmail.com>
2014-01-20 01:08:16 -0500

    add flot charts and db stuff

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"./lib/config"
 	_ "github.com/lib/pq"
 	"database/sql"
+	"strconv"
 )
 
 func runServer() {
@@ -25,7 +26,6 @@ func runServer() {
     r.HandleFunc("/", HomeHandler)
     r.HandleFunc("/ws", socket.WsHandler)
     r.HandleFunc("/public/{file:.*}", publicHandler)
-
     http.Handle("/", r)
     http.ListenAndServe(":3000", nil)
 }
@@ -36,25 +36,44 @@ func pullStocks() {
 		log.Fatal(err)
 	}
 	rows, err := db.Query("SELECT last_trade_price FROM stock_snapshot where stock_name = 'GOOG'")
-	fmt.Println(err)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(rows.Columns())
 	rows.Next()
 	var price float32
 	rows.Scan(&price)
 	fmt.Println(price)
 	for {
-		time.Sleep(5 * time.Second)
-		out, err := finance.GetStockInfo([]string{"GOOG", "MSFT"},[]string{finance.Last_Trade_Price_Only,finance.Price_Per_Earning_Ratio,finance.More_Info })
-	 	if(err!=nil){
-	 		fmt.Println(err)
-	 	}else{
-	 		fmt.Println(out)
+		if(true){
+			stockInfo, err := finance.GetStockInfo([]string{"GOOG", "FB"},[]string{finance.Last_Trade_Price_Only,finance.Price_Per_Earning_Ratio,finance.More_Info })
+		 	if(err!=nil){
+		 		fmt.Println(err)
+		 	}else{
+		 		fmt.Println(stockInfo)
+		 		for k, _ := range stockInfo { 
+		 			fmt.Println(stockInfo[k][finance.Last_Trade_Price_Only])
+		 		}
+		 	}
 	 	}
+	 	time.Sleep(5 * time.Second)
+	}
+}
+
+func stockMarketOpen(t time.Time)bool {
+	if (t.Weekday() >= 1 && t.Weekday() <= 5) && ((t.Hour() > 9 && t.Hour() <= 15) || (t.Hour() == 9 && t.Minute() >= 30)) {
+		return true
+	}else{
+		return false
 	}
 }
 
 func main() {
+	//runtime.GOMAXPROCS(runtime.NumCPU())
 	go runServer()
+	startTime := time.Now()
+	fmt.Println(stockMarketOpen(startTime))
+	fmt.Println("stockBrain started - "+strconv.Itoa(startTime.Hour()))
 	pullStocks()
 	/*fmt.Println("To stop enter q!")
 	reader := bufio.NewReader(os.Stdin)
